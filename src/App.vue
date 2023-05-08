d90354a36b1f7d7e0e30023439de7a8fae157d45
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-05-08 00:54:54 -0300

    nova formatação

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router'
-
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
@@ -16,41 +15,36 @@ import { RouterLink, RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-
-
 <style scoped>
-  header {
-    line-height: 1.5;
+header {
+  line-height: 1.5;
 
-    padding-top: 40px;
-    padding-bottom: 20px;
-  }
+  padding-top: 40px;
+  padding-bottom: 20px;
+}
 
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 0rem;
+  font-weight: 200;
 
-  nav {
-    width: 100%;
-    font-size: 12px;
-    text-align: center;
-    margin-top: 0rem;
-    font-weight: 200;
+  /* border: 1px solid red; */
+}
 
-    /* border: 1px solid red; */
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
 
-  }
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+  font-weight: 400;
+}
 
-  nav a.router-link-exact-active {
-    color: var(--color-text);
-  }
-
-  nav a.router-link-exact-active:hover {
-    background-color: transparent;
-    font-weight: 400;
-
-  }
-
-  nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-  }
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,28 @@
-<script>
-</script>
-
+<script></script>
 
 <template>
   <header>
     <div class="img-text">
       <img class="logo" src="@/assets/eu.jpeg" />
 
-      <div class="text"> Bem vindo!</div>
+      <div class="text">Bem vindo!</div>
       <div class="social-medias">
         <a href="https://instagram.com/bitakamori" target="_blank">
           Instagram
         </a>
-        <a href="https://www.facebook.com/bianca.takamori/" target="_blank">Facebok</a>
-        <a href="https://www.linkedin.com/in/bianca-helena-takamori-ribeiro-9557a5191/" target="_blank">Linkedin</a>
+        <a href="https://www.facebook.com/bianca.takamori/" target="_blank"
+          >Facebok</a
+        >
+        <a
+          href="https://www.linkedin.com/in/bianca-helena-takamori-ribeiro-9557a5191/"
+          target="_blank"
+          >Linkedin</a
+        >
         <a href="https://github.com/bitakamori" target="_blank">Github</a>
       </div>
     </div>
-
   </header>
 </template>
-
-
 
 <style scoped>
 .img-text {
@@ -34,27 +35,25 @@
   /* margin: 0 2rem; */
 }
 
-.text{
-  margin:  1rem 0;
+.text {
+  margin: 1rem 0;
 }
 
-.img-text>img {
+.img-text > img {
   width: 75%;
   border-radius: 50%;
   display: flex;
-
 }
 
 .social-medias {
   display: flex;
-  margin: .5rem auto;
+  margin: 0.5rem auto;
   height: 200px;
   justify-content: space-around;
   width: 50%;
 }
 
-.social-medias>a {
-  margin: 0 .5rem;
+.social-medias > a {
+  margin: 0 0.5rem;
 }
-
 </style>
